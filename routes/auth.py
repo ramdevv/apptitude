1c655115fc99ecabe69590b3b14ba1d034628f6d
ramdevv <garvitsharma0108@gmail.com>
2025-07-27 12:59:48 +0530

    added the login endpoint

--- routes/auth.py
+++ routes/auth.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter, Request
 from werkzeug.security import generate_password_hash, check_password_hash
-from utils import register_user
+from utils import (
+    register_user,
+    get_hashed_password,
+    check_user_by_email,
+    give_user_authtoken,
+)
 from models import CreateUser
+
 
 auth_router = APIRouter()
 
@@ -12,20 +18,39 @@ def register_new_user(users: CreateUser):
     name = users.name  # all the data which is passed through the CreateUser moodel will
     email = users.email  # get converted into the a users data object that i can acess
     user_password = users.password
+    print("before")
+    if check_user_by_email(email):
+        return {"error", "there is already a username with the entered email"}
+    else:
+        print("there is some one who is same ")
 
-    hashed_password = generate_password_hash(
-        user_password, method="pbkdf2:sha256", salt_length=16
-    )
-    # print(hashed_password)
+        hashed_password = generate_password_hash(
+            user_password, method="pbkdf2:sha256", salt_length=16
+        )
+        # print(hashed_password)
 
-    value = register_user(
-        users.name, users.email, hashed_password
-    )  # fill the user objects into the register fucntion
-    print(value)
+        value = register_user(
+            users.name, users.email, hashed_password
+        )  # fill the user objects into the register fucntion
+        print(value)
 
-    if isinstance(value, int):
-        print("the login is succesfull")
+        if isinstance(value, int):
+            print("the login is succesfull")
 
-    print(" the login was not succesfull")
+        print(" the login was not succesfull")
 
-    return {"name": name, "email": email}
+        return {"name": name, "email": email}
+
+
+@auth_router.post("/login")
+def login_user(users: CreateUser):
+    # take the input of the user
+    name = users.name
+    email = users.email
+    password = users.password  # this is the users password
+    # to get the users hashed password if there is any
+    if check_user_by_email(email):
+        hashed_pasword = get_hashed_password(email)
+        return give_user_authtoken(name)
+    else:
+        return {"error", "there is no use for this email "}
--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ from models import (
     CreateUser,
 )  # this is a pydantic model and it will be used to get the value of the createuser into the user modle which would be user to retrieve the data and update it
 
+import secrets
+
 
 def register_user(name: str, email: str, hashed_password: str):
     with connection.cursor() as cur:  # this will get us the acess to the db
@@ -13,3 +15,37 @@ def register_user(name: str, email: str, hashed_password: str):
         )
         connection.commit()  # commit needs to save the inserted values
         return cur.fetchone()
+
+
+def get_hashed_password(email: str):
+    with connection.cursor() as curr:
+        curr.execute("SELECT hashed_password FROM users WHERE email = %s", (email,))
+        result = curr.fetchone()
+        if result:
+            return result[0]
+        return None
+    # in this code we have added the comma after the email to treat this as a tupple and so we can add more parameters in the future
+
+
+def check_user_by_email(email: str):
+    with connection.cursor() as curr:
+        curr.execute("SELECT name FROM users WHERE email = %s", (email,))
+        result = curr.fetchone()
+        if result:
+            return result[0]
+        return None
+
+
+def give_user_authtoken(name: str):
+    login_token = secrets.token_urlsafe(32)  # this generates a random id
+    with connection.cursor() as curr:
+        curr.execute(
+            "UPDATE users SET auth_token = %s WHERE name = %s RETURNING name",
+            (login_token, name),
+        )
+        result = curr.fetchone()
+        connection.commit()  # this saves the response
+
+        if result:
+            return result[0]
+        return None
